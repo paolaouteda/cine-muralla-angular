2cd76179f2f3298ee5481bf8d8e07f772ea2621d
Paoprofile <paolaouteda@gmail.com>
2019-06-17 01:17:04 -0400

    doritos comida component

--- src/app/components/comida/comida.component.ts
+++ src/app/components/comida/comida.component.ts
@@ -7,19 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComidaComponent implements OnInit {
 
-  combos = [{combo: {
-    id:1,
-    nombre: 'combo de mentira'
-  }, comidas:  [{id: 1, categoria: 'chucheria', nombre: 'panqueca'}, {id: 2, categoria: 'dulce', nombre: 'torta'},{id: 3, categoria: 'salado', nombre: 'mani'}]
-},{combo: {
-  id:2,
-  nombre: 'combo de mentira 2'
-}, comidas:  [{id: 3, categoria: 'chucheria', nombre: 'doritos'}, {id: 4, categoria: 'dulce', nombre: 'pastel'}]
-}, {combo: {
-  id:3,
-  nombre: 'combo de mentira 3'
-}, comidas:  [{id: 4, categoria: 'salado', nombre: 'Omar'}]
-}];
+  combos = [{
+    combo: {
+      id: 1,
+      nombre: 'combo de mentira'
+    },
+     comidas: [{ id: 1, categoria: 'chucheria', nombre: 'panqueca', cantidad: 2 },
+     { id: 2, categoria: 'dulce', nombre: 'torta', cantidad: 5 },
+    { id: 3, categoria: 'salado', nombre: 'mani', cantidad: 20 }]
+  }, {
+    combo: {
+      id: 2,
+      nombre: 'combo de mentira 2'
+    }, comidas: [{ id: 3, categoria: 'chucheria', nombre: 'doritos', cantidad: 10 },
+     { id: 4, categoria: 'dulce', nombre: 'pastel', cantidad: 99999 }]
+  }, {
+    combo: {
+      id: 3,
+      nombre: 'combo de mentira 3'
+    }, comidas: [{ id: 4, categoria: 'salado', nombre: 'Omar', cantidad: 999999999999999999999999999999999999999999999999999999999999999999999999999 }]
+  }];
 
   constructor() { }
 
