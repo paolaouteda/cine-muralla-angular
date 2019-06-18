143cdf89331cb637f27e39733556f57ddc2e585f
Paola Outeda <paolaouteda@gmail.com>
2019-06-17 22:31:53 -0400

    butacas validation

--- src/app/components/butacas/butacas.component.ts
+++ src/app/components/butacas/butacas.component.ts
@@ -54,9 +54,15 @@ export class ButacasComponent implements OnInit {
     let index = this.butacasCompradas.findIndex(
       butaca =>
         butaca.fila === fila &&
-        (butaca.columna == columna + 1 || butaca.columna - 1)
+        (butaca.columna == columna + 1 || butaca.columna == columna - 1)
     );
-    if (this.butacasCompradas.length > 0 && index !== -1) {
+    if (
+      this.butacasCompradas.length > 0 &&
+      index !== -1 &&
+      !this.butacasCompradas.some(
+        butaca => butaca.fila === fila && butaca.columna === columna
+      )
+    ) {
       this.butacasCompradas.push({ fila, columna });
       this.salaButacas.butacas[fila - 1][columna] = !this.salaButacas.butacas[
         fila - 1
@@ -73,9 +79,19 @@ export class ButacasComponent implements OnInit {
           butaca => butaca.fila === fila && butaca.columna == columna
         );
         if (index !== -1) {
-          this.salaButacas.butacas[fila - 1][columna] = !this.salaButacas
-            .butacas[fila - 1][columna];
-          this.butacasCompradas.splice(index, 1);
+          let init = this.butacasCompradas.findIndex(
+            butaca => butaca.fila === fila && butaca.columna == columna - 1
+          );
+          let finish = this.butacasCompradas.findIndex(
+            butaca => butaca.fila === fila && butaca.columna == columna + 1
+          );
+          if (init != -1 && finish != -1) {
+            alert("Seleccione una butaca de los extremos");
+          } else {
+            this.salaButacas.butacas[fila - 1][columna] = !this.salaButacas
+              .butacas[fila - 1][columna];
+            this.butacasCompradas.splice(index, 1);
+          }
         } else alert("Por favor seleccione un asiento contiguo");
       }
     }
