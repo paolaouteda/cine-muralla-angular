b874dec8722bbb7bffa180356f2952b033124b06
Paola Outeda <paolaouteda@gmail.com>
2019-06-18 21:18:45 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { Routes, RouterModule } from '@angular/router';
-import { CinesComponent } from './components/cines/cines.component';
-import { AdministradorComponent } from './components/administrador/administrador.component';
-import { ComidaComponent } from './components/comida/comida.component';
-import { CarteleraComponent } from './components/cartelera/cartelera.component';
-import { ButacasComponent } from './components/butacas/butacas.component';
-import { DataService } from './services/dataService';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { Routes, RouterModule } from "@angular/router";
+import { CinesComponent } from "./components/cines/cines.component";
+import { AdministradorComponent } from "./components/administrador/administrador.component";
+import { ComidaComponent } from "./components/comida/comida.component";
+import { CarteleraComponent } from "./components/cartelera/cartelera.component";
+import { ButacasComponent } from "./components/butacas/butacas.component";
+import { DataService } from "./services/dataService";
+import { DashboardComponent } from "./dashboard/dashboard.component";
 
 const appRoutes: Routes = [
-  { path: 'cines', component: CinesComponent },
-  { path: 'administrador', component: AdministradorComponent },
+  { path: "", component: DashboardComponent },
+  { path: "cines", component: CinesComponent },
+  { path: "administrador", component: AdministradorComponent },
   {
-    path: 'comida',
+    path: "comida",
     component: ComidaComponent
   },
   {
-    path: 'cartelera/:nombreFiscal',
+    path: "cartelera/:nombreFiscal",
     component: CarteleraComponent
   },
-  { path: 'butacas/:id', component: ButacasComponent }
+  { path: "butacas/:id", component: ButacasComponent }
 ];
 
 @NgModule({
@@ -33,9 +35,15 @@ const appRoutes: Routes = [
     AdministradorComponent,
     ComidaComponent,
     CarteleraComponent,
-    ButacasComponent
+    ButacasComponent,
+    DashboardComponent
   ],
-  imports: [RouterModule.forRoot(appRoutes), BrowserModule, AppRoutingModule, NgbModule],
+  imports: [
+    RouterModule.forRoot(appRoutes),
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
   providers: [DataService],
   bootstrap: [AppComponent]
 })
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,106 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.css"]
+})
+export class DashboardComponent implements OnInit {
+  images = [1, 2, 3].map(
+    () => `https://picsum.photos/900/500?random&t=${Math.random()}`
+  );
+  funciones = [
+    {
+      horarioInicio: new Date(),
+      horarioFin: new Date(),
+      sala: {
+        id: 1,
+        tipo: "premium",
+        repro: "3D"
+      },
+      pelicula: {
+        id: 1,
+        duracion: 120,
+        titulo: "Pepo y los pepos magicos",
+        descripcion:
+          "Pelicula de ejemplo basada en la noche en la que que pepo preferia madrugar y trabajar en esto que dormir con los ronquidos de Armando",
+        imagen: "https://i.blogs.es/5efe2c/cap_001/450_1000.jpg",
+        genero: "Accion",
+        censura: "A",
+        fechaEstreno: new Date()
+      },
+      idioma: "ingles",
+      subtitulos: "espanol"
+    },
+    {
+      horarioInicio: new Date(),
+      horarioFin: new Date(),
+      sala: {
+        id: 2,
+        tipo: "normal",
+        repro: "4DX"
+      },
+      pelicula: {
+        id: 2,
+        duracion: 120,
+        titulo: "Attack on Titan",
+        imagen:
+          "https://cdn.gbposters.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/a/t/attack-on-titan-season-2-key-art-maxi-poster-1.133.jpg",
+        descripcion:
+          "pelicula de ejemplo basada en la noche en la que que pepo preferia madrugar y trabajar en esto que dormir con los ronquidos de Armando",
+        genero: "Accion",
+        censura: "A",
+        fechaEstreno: new Date()
+      },
+      idioma: "ingles",
+      subtitulos: "espanol"
+    },
+    {
+      horarioInicio: new Date(),
+      horarioFin: new Date(),
+      sala: {
+        id: 3,
+        tipo: "premium",
+        repro: "3D"
+      },
+      pelicula: {
+        id: 1,
+        duracion: 120,
+        titulo: "Pepo y los pepos magicos",
+        descripcion:
+          "Pelicula de ejemplo basada en la noche en la que que pepo preferia madrugar y trabajar en esto que dormir con los ronquidos de Armando",
+        imagen: "https://i.blogs.es/5efe2c/cap_001/450_1000.jpg",
+        genero: "Accion",
+        censura: "A",
+        fechaEstreno: new Date()
+      },
+      idioma: "ingles",
+      subtitulos: "espanol"
+    },
+    {
+      horarioInicio: new Date(),
+      horarioFin: new Date(),
+      sala: {
+        id: 4,
+        tipo: "premium",
+        repro: "3D"
+      },
+      pelicula: {
+        id: 1,
+        duracion: 120,
+        titulo: "Pepo y los pepos magicos",
+        descripcion:
+          "Pelicula de ejemplo basada en la noche en la que que pepo preferia madrugar y trabajar en esto que dormir con los ronquidos de Armando",
+        imagen: "https://i.blogs.es/5efe2c/cap_001/450_1000.jpg",
+        genero: "Accion",
+        censura: "A",
+        fechaEstreno: new Date()
+      },
+      idioma: "ingles",
+      subtitulos: "espanol"
+    }
+  ];
+  constructor() {}
+
+  ngOnInit() {}
+}
